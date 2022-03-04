45848ea492fcd63ce9741310030a0d2e45ce2ba4
Mayant Mukul <mayantmukul@outlook.in>
2022-03-04 23:24:33 +0530

    [scheme-hs] parse with Parsec

--- scheme-hs/app/Main.hs
+++ scheme-hs/app/Main.hs
@@ -1,4 +1,83 @@
 module Main where
 
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- Define a type for parser output
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+-- Parser for symbols allowed in Scheme identifiers
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+-- Recognise spaces, and ignore them by carrying Unit type with Parser
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return (String x)
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return
+    ( case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _ -> Atom atom
+    )
+
+parseNumber :: Parser LispVal
+parseNumber = fmap (Number . read) (many1 digit)
+
+parseList :: Parser LispVal
+parseList = fmap List (sepBy parseExpr spaces)
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return (DottedList head tail)
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  char '\''
+  return (List [Atom "quote", x])
+
+parseExpr :: Parser LispVal
+parseExpr =
+  parseAtom
+    <|> parseString
+    <|> parseNumber
+    <|> parseQuoted
+    <|> do
+      char '('
+      x <- try parseList <|> parseDottedList
+      char ')'
+      return x
+
+-- Entry point to parse an expression
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
